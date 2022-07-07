2b6617f8c2fd310ada16de345e69ea26b7bdf27b
StevenLares <steven.lares@uconn.edu>
2022-07-06 20:27:04 -0400

    Finished the hashtable implementation, for now. Will likely fix issues later while testing. I also added a driver class

--- src/HashTable.java
+++ src/HashTable.java
@@ -25,8 +25,33 @@ public class HashTable {
     }
 
 
+
+    public Boolean keyExists(String key){
+        int index = compute(key);
+
+        if(hashIndicies.get(index) == null){ //the computed index does not have any key,value pairs
+            return false;
+        }
+        else {
+            LinkedList<HashEntry> linkedList = hashIndicies.get(index);
+            for (HashEntry hashEntry : linkedList) {
+                if(hashEntry.getKey() == key){
+                    return true;
+                }
+            }
+        }
+
+        return false;
+
+
+    }
+
+
+
+
     //this function adds a key / value pair to the hashtable.
-    public void add(String key, int value, boolean replace){
+    //does not add if the key already exists
+    public void add(String key, int value){
         HashEntry hashEntry = new HashEntry(key, value);
         int index = compute(key);
 
@@ -34,36 +59,78 @@ public class HashTable {
         if(hashIndicies.get(index) == null){
             LinkedList<HashEntry> linkedList = new LinkedList<HashEntry>();
             hashIndicies.set(index, linkedList); //create new linkedList, add the key there.
-        } else { //index has entries
-            //TODO: Implement functionality
+        } else if (keyExists(key) == false){
+            LinkedList<HashEntry> linkedList = hashIndicies.get(index); //create ref. to linkedList at this index
+            linkedList.add(hashEntry); //add the hashEntry to the linkedList
+        } else System.out.println("Sorry, this key already exists in the hashtable./n " +
+                "Please use the update(), remove(), or getValue() methods with this key as a parameter.");
 
+
+    }
+
+
+
+    public void update(String key, int newVal){
+        if(keyExists(key) == false){
+            System.out.println("Sorry, this key does not exist in the hashtable./n There is nothing to update.");
+        }
+        else {
+            int index = compute(key);
+            LinkedList<HashEntry> linkedList = hashIndicies.get(index);
+            for (HashEntry hashEntry : linkedList) {
+                if(hashEntry.getKey() == key){
+                    int oldValue = hashEntry.getValue();
+                    hashEntry.setValue(newVal);
+                    System.out.println("Key " + key + ": Value updated from " + oldValue + " to " + newVal);
+                    break;
+                }
+            }
+        }
+
+    }
+
+
+
+
+    public void remove(String key){
+        if(keyExists(key) == false){
+            System.out.println("Sorry, this key does not exist in the hashtable./n There is nothing to remove.");
+        }
+        else {
+            int index = compute(key);
+            LinkedList<HashEntry> linkedList = hashIndicies.get(index);
+            for (HashEntry hashEntry : linkedList) {
+                if(hashEntry.getKey() == key){
+                    int value = hashEntry.getValue();
+                    linkedList.remove(hashEntry);
+                    System.out.println("Key / Value pair was removed from hashtable: " + key + " : " + value);
+                    break;
+                }
+            }
         }
 
 
-
-    }
-
-
-    public void replace(){
-
     }
 
 
 
+    //returns value given a key
+    //returns null if the key is not in the hashtable
     public Integer getValue(String key){
-        int index = compute(key);
-
-        if(hashIndicies.get(index) != null){
+        if(keyExists(key) == false){
+            System.out.println("Sorry, this key does not exist in the hashtable./n There is no value to retrieve.");
+            return null;
+        }
+        else {
+            int index = compute(key);
             LinkedList<HashEntry> linkedList = hashIndicies.get(index);
             for (HashEntry hashEntry : linkedList) {
                 if(hashEntry.getKey() == key){
                     return hashEntry.getValue();
                 }
             }
-
         }
         return null;
-
 
     }
 
---
+++ src/driver_class.java
@@ -0,0 +1,11 @@
+/*
+* This will be the main class where functions will be run.
+* */
+
+public class driver_class {
+
+    public static void main(String[] args){
+
+    }
+
+}
